1b905e3b660d896c34e2a0eac1525256b11867ca
Rails Learner <xborys0@gmail.com>
2012-07-09 21:25:36 +0200

    Exercises #1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector( 'h1', :text => 'Sample App' )
     end
@@ -17,7 +17,7 @@ describe "Static Pages" do
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_selector( 'h1', :text => 'Help' )
     end
@@ -30,7 +30,7 @@ describe "Static Pages" do
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector( 'h1', :text => 'About Us' )
     end
@@ -38,6 +38,19 @@ describe "Static Pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector( 'title', :text => '| About Us' )
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector( 'h1', :text => 'Contact' )
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector( 'title', :text => '| Contact' )
     end
   end
 end
