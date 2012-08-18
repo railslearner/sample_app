eec1094d658b5b55025ec4a34c40d2efe4e40abf
Rails Learner <xborys0@gmail.com>
2012-08-18 14:43:32 +0200

    Exercises #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [ :index, :edit, :update, :destroy ]
   before_filter :correct_user, only: [ :edit, :update ]
   before_filter :admin_user, only: :destroy
+  before_filter :unsigned_in_user, only: [ :new, :create ]
 
   def show
     @user = User.find(params[:id])
@@ -63,5 +64,9 @@ class UsersController < ApplicationController
 
   def admin_user
     redirect_to(root_path) unless current_user.admin?
+  end
+
+  def unsigned_in_user
+    redirect_to root_path, notice: "You're already signed in." if signed_in?
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,9 +28,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        sign_in user
       end
 
       it { should have_selector('title', text: user.name) }
@@ -56,15 +54,21 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
 
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
+          end
+
+          describe "when signing in again" do
+            before { sign_in user }
+
+            it "should render the default (profile) page" do
+              page.should have_selector('title', text: user.name) 
+            end
           end
         end
       end
